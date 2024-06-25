373bb2e93253585a80ae7f0e92103321384ac7f3
vishwa <78722643+vsgames42@users.noreply.github.com>
2024-06-25 10:01:14 +0530

    Add files via upload

---
+++ js/career.js
@@ -0,0 +1,133 @@
+$(document).ready(function(){
+    $('#panel').slideUp()
+    $('#flip').click(function(){
+        $('#panel').slideDown("slow")
+        $('#ar').animate({
+            rotate: '90deg'
+        })
+    })
+    $('#flip').dblclick(function(){
+        $('#panel').slideUp("slow")
+        $('#ar').animate({rotate:'0deg'})
+    })
+})
+$(document).ready(function(){
+    $('#panel1').slideUp()
+    $('#flip1').click(function(){
+        $('#panel1').slideDown("slow")
+        $('#ar1').animate({
+            rotate: '90deg'
+        })
+    })
+    $('#flip1').dblclick(function(){
+        $('#panel1').slideUp("slow")
+        $('#ar1').animate({rotate:'0deg'})
+    })
+})
+$(document).ready(function(){
+    $('#panel2').slideUp()
+    $('#flip2').click(function(){
+        $('#panel2').slideDown("slow")
+        $('#ar2').animate({
+            rotate: '90deg'
+        })
+    })
+    $('#flip2').dblclick(function(){
+        $('#panel2').slideUp("slow")
+        $('#ar2').animate({rotate:'0deg'})
+    })
+})
+$(document).ready(function(){
+    $('#panel3').slideUp()
+    $('#flip3').click(function(){
+        $('#panel3').slideDown("slow")
+        $('#ar3').animate({
+            rotate: '90deg'
+        })
+    })
+    $('#flip3').dblclick(function(){
+        $('#panel3').slideUp("slow")
+        $('#ar3').animate({rotate:'0deg'})
+    })
+})
+$(document).ready(function(){
+    $('#panel4').slideUp()
+    $('#flip4').click(function(){
+        $('#panel4').slideDown("slow")
+        $('#ar4').animate({
+            rotate: '90deg'
+        })
+    })
+    $('#flip4').dblclick(function(){
+        $('#panel4').slideUp("slow")
+        $('#ar4').animate({rotate:'0deg'})
+    })
+})
+$(document).ready(function(){
+    $('#panel5').slideUp()
+    $('#flip5').click(function(){
+        $('#panel5').slideDown("slow")
+        $('#ar5').animate({
+            rotate: '90deg'
+        })
+    })
+    $('#flip5').dblclick(function(){
+        $('#panel5').slideUp("slow")
+        $('#ar5').animate({rotate:'0deg'})
+    })
+})
+$(document).ready(function(){
+    $('#panel6').slideUp()
+    $('#flip6').click(function(){
+        $('#panel6').slideDown("slow")
+        $('#ar6').animate({
+            rotate: '90deg'
+        })
+    })
+    $('#flip6').dblclick(function(){
+        $('#panel6').slideUp("slow")
+        $('#ar6').animate({rotate:'0deg'})
+    })
+})
+$(document).ready(function(){
+    $('#panel7').slideUp()
+    $('#flip7').click(function(){
+        $('#panel7').slideDown("slow")
+        $('#ar7').animate({
+            rotate: '90deg'
+        })
+    })
+    $('#flip7').dblclick(function(){
+        $('#panel7').slideUp("slow")
+        $('#ar7').animate({rotate:'0deg'})
+    })
+})
+$(document).ready(function(){
+    $('#panel8').slideUp()
+    $('#flip8').click(function(){
+        $('#panel8').slideDown("slow")
+        $('#ar8').animate({
+            rotate: '90deg'
+        })
+    })
+    $('#flip8').dblclick(function(){
+        $('#panel8').slideUp("slow")
+        $('#ar8').animate({rotate:'0deg'})
+    })
+})
+$(document).ready(function(){
+    $('#panel9').slideUp()
+    $('#flip9').click(function(){
+        $('#panel9').slideDown("slow")
+        $('#ar9').animate({
+            rotate: '90deg'
+        })
+    })
+    $('#flip9').dblclick(function(){
+        $('#panel9').slideUp("slow")
+        $('#ar9').animate({rotate:'0deg'})
+    })
+})
+$(document).ready(function() {
+    $("#lightSlider").lightSlider(); 
+});
